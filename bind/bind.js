8b1599d5bd9d3ff64e1206a78b969741d90c644b
gdi2290 <github@gdi2290.com>
2013-08-05 15:51:25 -0700

    create bind with example code

--- bind/bind.js
+++ bind/bind.js
@@ -1,20 +1,17 @@
-/**
- * Write a function that sets the keyword `this` to a specified value.
- *
- * Example:
- *
- * var alice = {
- *   name: 'alice',
- *   shout: function(){
- *     alert(this.name);
- *   }
- * }
- * var boundShout = bind(alice.shout, alice);
- * boundShout(); // alerts 'alice'
- * boundShout = bind(alice.shout, {name: 'bob'});
- * boundShout(); // alerts 'bob'
- */
-
-var bind = function() {
-
+var bind = function(method, context) {
+  return function(args) {
+    args = args && [].slice.call(args);
+    method.apply(context, args);
+  };
 };
+
+ var alice = {
+   name: 'alice',
+   shout: function(){
+     alert(this.name);
+   }
+ };
+ var boundShout = bind(alice.shout, alice);
+ boundShout(); // alerts 'alice'
+ boundShout = bind(alice.shout, {name: 'bob'});
+ boundShout(); // alerts 'bob'
