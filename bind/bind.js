25692a535a3c69a78f3cb4932e68ca9796c363f2
gdi2290 <github@gdi2290.com>
2013-08-05 15:48:13 -0700

    setup bind problem

---
+++ bind/bind.js
@@ -0,0 +1,20 @@
+/**
+ * Write a function that sets the keyword `this` to a specified value.
+ *
+ * Example:
+ *
+ * var alice = {
+ *   name: 'alice',
+ *   shout: function(){
+ *     alert(this.name);
+ *   }
+ * }
+ * var boundShout = bind(alice.shout, alice);
+ * boundShout(); // alerts 'alice'
+ * boundShout = bind(alice.shout, {name: 'bob'});
+ * boundShout(); // alerts 'bob'
+ */
+
+var bind = function() {
+
+};
