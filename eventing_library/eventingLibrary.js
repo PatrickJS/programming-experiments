cdb911d68e5a3f14e790d0b57f8976216b200721
gdi2290 <github@gdi2290.com>
2013-08-05 16:46:57 -0700

    wrap inside a closure for data hiding

--- eventing_library/eventingLibrary.js
+++ eventing_library/eventingLibrary.js
@@ -1,40 +1,39 @@
-/**
- * Make an eventing system mix-in that adds .trigger() and .on() to any input
- * object.
- */
+;(function() {
 
-var mixEvents = function(obj) {
-  obj._events = {};
-  obj.on = function(event, callback) {
-    if (!obj._events[event]) {
-      obj._events[event] = [];
+  var mixEvents = function(obj) {
+    obj._events = {};
+    obj.on = function(event, callback) {
+      if (!obj._events[event]) {
+        obj._events[event] = [];
+      }
+      obj._events[event].push(callback);
     }
-    obj._events[event].push(callback);
-  }
-  obj.trigger = function(event, args) {
-    for (var i = 0; i < obj._events[event].length; i++) {
-      obj._events[event][i].apply(obj, args);
+    obj.trigger = function(event, args) {
+      for (var i = 0; i < obj._events[event].length; i++) {
+        obj._events[event][i].apply(obj, args);
+      }
     }
-  }
-  return obj;
-};
+    return obj;
+  };
 
 
-// Consumption code
-var obj = {
-  name: 'Alice',
-  age: 30
-};
+  // Consumption code
+  var obj = {
+    name: 'Alice',
+    age: 30
+  };
 
-mixEvents(obj);
+  mixEvents(obj);
 
-obj.on('ageChange', function(){
-  ++this.age;
-  console.log('Age changed', this.age);
-});
+  obj.on('ageChange', function(){
+    ++this.age;
+    console.log('Age changed', this.age);
+  });
 
-obj.trigger('ageChange');
-obj.trigger('ageChange');
-obj.trigger('ageChange');
-// => Should log 'Age changed'
+  obj.trigger('ageChange');
+  obj.trigger('ageChange');
+  obj.trigger('ageChange');
+  // => Should log 'Age changed'
 
+
+}())
