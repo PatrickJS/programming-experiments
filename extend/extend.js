d3567de26079681c742a1a2c94edad605f985845
gdi2290 <github@gdi2290.com>
2013-08-05 17:10:21 -0700

    update extend with pjs

--- extend/extend.js
+++ extend/extend.js
@@ -12,23 +12,34 @@
  * Write a shallow-copy version of extend, and then write a deep-copy version.
  * Make your deep-copy extend handle an arbitrary number of source objects.
  */
+window.pJS = pJS || {};
 
-
-
-
-var extend = function(obj, args) {
-  args = Array.prototype.slice.call(arguments, 1);
-  for (var i = 0; i < args.length; i++) {
-    for (var key in args[i]) {
-      if (typeof args[i][key] === 'object') {
-        extend(obj, args[i][key])
-      } else {
+;(function(pjs) {
+  pjs.extend = function(obj, args) {
+    args = Array.prototype.slice.call(arguments, 1);
+    for (var i = 0; i < args.length; i++) {
+      for (var key in args[i]) {
         obj[key] = args[i][key];
       }
     }
-  }
-};
+    return obj;
+  };
 
+  pjs.deepExtend = function(obj, args) {
+    args = Array.prototype.slice.call(arguments, 1);
+    for (var i = 0; i < args.length; i++) {
+      for (var key in args[i]) {
+        if (typeof args[i][key] === 'object') {
+          extend(obj, args[i][key])
+        } else {
+          obj[key] = args[i][key];
+        }
+      }
+    }
+    return obj;
+  };
+
+}(pJS));
 
 ;(function() {
   var person = {
@@ -48,7 +59,9 @@ var extend = function(obj, args) {
     rightLeg: 'right-leg'
   };
 
-  extend(person, arms, legs);
-  console.log(person);
+  var firstPerson = pJS.extend(person, arms, legs);
+  console.log(firstPerson);
+  var newPerson = pJS.deepExtend({}, person, arms, legs);
+  console.log(newPerson);
 
 }());
